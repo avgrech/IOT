bf9d0c177d68afdbf3c2b6b9821e3afdc190b9da
avgrech <avggrech@gmail.com>
2025-06-30 09:12:23 +0200

    Improve Blazor UI

---
+++ HomeAutomationBlazor/Models/Organisation.cs
@@ -0,0 +1,7 @@
+namespace HomeAutomationBlazor.Models;
+
+public class Organisation
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ HomeAutomationBlazor/Models/Property.cs
@@ -0,0 +1,9 @@
+namespace HomeAutomationBlazor.Models;
+
+public class Property
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string RouterDeviceId { get; set; } = string.Empty;
+    public int OrganisationId { get; set; }
+}
