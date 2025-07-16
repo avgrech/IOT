07f741fe166bba191412902fbfe23e54be2a2f02
Andrew Grech <andrew.grech@2iltd.com>
2025-07-16 13:11:29 +0200

    migration

--- HomeAuthomationAPI/Migrations/20250716102039_YourMigrationName.Designer.cs
+++ HomeAuthomationAPI/Migrations/20250716102039_YourMigrationName.Designer.cs
@@ -4,6 +4,7 @@ using HomeAuthomationAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeAuthomationAPI.Migrations
 {
     [DbContext(typeof(HomeAutomationContext))]
-    partial class HomeAutomationContextModelSnapshot : ModelSnapshot
+    [Migration("20250716102039_YourMigrationName")]
+    partial class YourMigrationName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
