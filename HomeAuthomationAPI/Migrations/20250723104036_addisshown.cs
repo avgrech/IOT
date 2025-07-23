fc7a4e8a3c77659d14e4336447c2aaf6cae17dc2
Andrew Grech <andrew.grech@2iltd.com>
2025-07-23 12:44:01 +0200

    changes to migration

---
+++ HomeAuthomationAPI/Migrations/20250723104036_addisshown.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeAuthomationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addisshown : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShown",
+                table: "Parameters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsShown",
+                table: "Parameters");
+        }
+    }
+}
