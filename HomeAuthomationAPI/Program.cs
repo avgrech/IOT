fcc8b4f17b3f9ddde4c969bf172d6dd9af01b16a
avgrech <avggrech@gmail.com>
2025-07-24 09:18:24 +0200

    Add integration tests for API

--- HomeAuthomationAPI/Program.cs
+++ HomeAuthomationAPI/Program.cs
@@ -17,13 +17,20 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Home Automation API", Version = "v1" });
 });
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-Console.WriteLine(">>> EF is using connection string: " + connectionString);
-
-builder.Services.AddDbContext<HomeAutomationContext>(options =>
-    options.UseSqlServer(connectionString)
-           .EnableSensitiveDataLogging()
-           .LogTo(Console.WriteLine, LogLevel.Information));
+if (builder.Environment.IsEnvironment("Testing"))
+{
+    builder.Services.AddDbContext<HomeAutomationContext>(options =>
+        options.UseInMemoryDatabase("TestDb"));
+}
+else
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    Console.WriteLine(">>> EF is using connection string: " + connectionString);
+    builder.Services.AddDbContext<HomeAutomationContext>(options =>
+        options.UseSqlServer(connectionString)
+               .EnableSensitiveDataLogging()
+               .LogTo(Console.WriteLine, LogLevel.Information));
+}
 
 
 var app = builder.Build();
